73c9b09cbcfdf280de6b3a275ea16fbb6201b605
adityaakash31-pixel <adityaakash31@gmail.com>
2026-07-04 20:46:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function startApp() {
     window.location.href = "dashboard.html";
 }
+function saveIncome() {
+    let source = document.getElementById("source").value;
+    let amount = document.getElementById("amount").value;
+
+    if (source === "" || amount === "") {
+        alert("Please fill all fields");
+        return;
+    }
+
+    let list = document.getElementById("incomeList");
+
+    let item = document.createElement("li");
+    item.innerHTML = source + " - ₹" + amount;
+
+    list.appendChild(item);
+
+    document.getElementById("source").value = "";
+    document.getElementById("amount").value = "";
+}
 const saveButton = document.querySelector("button");
 
 if (saveButton) {
