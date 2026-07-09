7107479eb84b03ebe165ee929abdae719f786f03
adityaakash31-pixel <adityaakash31@gmail.com>
2026-07-09 14:22:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-console.log("Aakash Finance V4 Loaded Successfully!");
+// Aakash Finance V4
+
+let totalIncome = Number(localStorage.getItem("totalIncome")) || 0;
 
 window.onload = function () {
-    alert("Welcome to Aakash Finance V4");
+
+    let incomeBox = document.getElementById("totalIncome");
+
+    if (incomeBox) {
+        incomeBox.innerText = totalIncome;
+    }
+
 };
+
+function saveIncome() {
+
+    let source = document.getElementById("source").value;
+    let amount = Number(document.getElementById("amount").value);
+
+    if (source === "") {
+        alert("Income Category Select kijiye");
+        return;
+    }
+
+    if (amount <= 0) {
+        alert("Amount Enter kijiye");
+        return;
+    }
+
+    totalIncome += amount;
+
+    localStorage.setItem("totalIncome", totalIncome);
+
+    document.getElementById("totalIncome").innerText = totalIncome;
+
+    alert("✅ Income Saved Successfully");
+
+    document.getElementById("source").value = "";
+    document.getElementById("amount").value = "";
+
+}
