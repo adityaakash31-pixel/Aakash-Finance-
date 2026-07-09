0eda0eeb04b168b564cab1ba020e145c967d0e76
adityaakash31-pixel <adityaakash31@gmail.com>
2026-07-09 21:24:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,3 +53,64 @@ window.onload = function () {
 };
 
 console.log("Aakash Finance V4 Part 1 Loaded");
+// ===============================
+// SAVE INCOME
+// ===============================
+
+function saveIncome() {
+
+    let source = document.getElementById("source").value;
+    let amount = Number(document.getElementById("amount").value);
+
+    if (source == "") {
+        alert("Income Category Select kijiye");
+        return;
+    }
+
+    if (amount <= 0) {
+        alert("Amount Enter kijiye");
+        return;
+    }
+
+    let data = {
+        source: source,
+        amount: amount
+    };
+
+    incomeHistory.push(data);
+
+    localStorage.setItem(
+        "incomeHistory",
+        JSON.stringify(incomeHistory)
+    );
+
+    totalIncome += amount;
+
+    localStorage.setItem(
+        "totalIncome",
+        totalIncome
+    );
+
+    let totalBox = document.getElementById("totalIncome");
+    if (totalBox) {
+        totalBox.innerText = totalIncome;
+    }
+
+    let list = document.getElementById("incomeList");
+
+    if (list) {
+
+        let li = document.createElement("li");
+
+        li.innerHTML =
+            source + " - ₹" + amount;
+
+        list.appendChild(li);
+    }
+
+    document.getElementById("source").value = "";
+    document.getElementById("amount").value = "";
+
+    alert("✅ Income Saved Successfully");
+
+}
